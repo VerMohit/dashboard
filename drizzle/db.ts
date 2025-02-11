ddc17fe97c6b07fd0c2658ed329de335edb26169
Mohit Verma <mohitver01@gmail.com>
2025-02-11 16:57:12 -0500

    Added github actions workflow file 'githubCI.yml'

--- drizzle/db.ts
+++ drizzle/db.ts
@@ -1,8 +1,15 @@
 import { neon } from '@neondatabase/serverless';
 import { config } from 'dotenv';
 import { drizzle } from 'drizzle-orm/neon-http';
+import { Customer } from './schema';
 
 config();
+
+if (!process.env.DATABASE_URL) {
+    console.error('DATABASE_URL is not defined in .env');
+    process.exit(1);  // Exit if the DATABASE_URL is missing
+}
+
 
 const sql = neon(process.env.DATABASE_URL!);   // Use the connection string from .env
 export const db = drizzle({ client: sql });    // Initialize Drizzle ORM with Neon Postgres client
